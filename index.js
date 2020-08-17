ec7eebd289eb6b46116b3338372d0fb621f33a54
Bridgette-theWebMaster <69090403+Bridgette-theWebMaster@users.noreply.github.com>
2020-08-17 14:16:57 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,150 @@
+'use strict';
+// defining a few variable used throughout
+let shots = 5;
+let questionNum = 0;
+let currentQuestion = 0;
+
+// when the user clicks the begin button
+function startQuiz() {
+    $('.altBox').hide();
+    $('.beginQuiz').on('click', '.startButton', function (event){
+        $('.startButton').hide();
+        $('.final').hide();
+        $('.questionNum').text(1);
+        $('.questionBox').show();
+        $('.questionBox').prepend(renderQuestions());
+    });
+}
+// display question
+function renderQuestions(){
+    if(questionNum < STORE.length){
+        return questionForm(questionNum);
+    }else{
+        $('.questionBox').hide();
+        endQuiz();
+        $('.questionNum').text(5);
+    }
+}
+
+// display question number and shots to take
+function updateQuestion(){
+    questionNum++;
+    $('.questionNum').text(questionNum + 1);
+}
+function updateShots(){    
+    shots--;
+    $('.shots').text(shots);
+}
+
+function resetQuiz(){
+    shots = 5;
+    questionNum = 0;
+    $('.shots').text(5);
+    $('.questionNum').text(0);
+}
+
+function submitAnswer(){
+    $(".theBar").on('submit', function(event){
+        event.preventDefault();
+        $('.altBox').hide();
+        $('.response').show();
+        let selected = $('input:checked');
+        let answer = selected.val();
+        let correct = STORE[questionNum].correctAnswer;
+        if (answer === correct){
+            correctAnswer();
+        }else{
+            wrongAnswer();
+        }
+    });
+}
+
+// html question form
+function questionForm(questionIndex){
+    let formMaker = $(`<form>
+    <fieldset>
+        <legend class= "questionText">${STORE[questionIndex].question}
+        </legend>
+    </fieldset>
+</form>`)
+    let answerSelector = $(formMaker).find('fieldset');
+    STORE[questionIndex].answer.forEach(function (answerValue, answerIndex){
+        $(`<label class="bookMark" for=${answerIndex}">
+        <input class= "radio" type= "radio" id= "${answerIndex}" value= "${answerValue}" name= "answer" required>
+        <span>${answerValue}</span>
+        </label>`).appendTo(answerSelector);
+    });
+    $(`<button type= "submit" class= "submitButton button">Submit</button>`).appendTo(answerSelector);
+    return formMaker;
+}
+// correct and incorrect reponse
+function correctAnswer(){
+    $('.response').html(`
+        <h3>You're right!</h3>
+        <button type= "button" class= "nextButton button">Next</button>
+    `);
+}
+function wrongAnswer(){
+    $('.reponse').html(`
+        <h3>Your answer is not correct.</h3>
+        <p>The correct answer is:</p>
+        <p>${STORE[questionNum].correctAnswer}</p>
+        <button type= "button" class= "nextButton button">Next</button>
+    `);    
+    updateShots();
+}
+
+// move on to the next question
+function nextQuestion(){
+    $('.theBar').on('click', 'nextButton', function(event){
+        $('.altBox').hide();
+        $('.questionBox').show();
+        updateQuestionNum();
+        $('.questionBox form').replaceWith(renderQuestions());
+    });
+}
+
+// display results
+function displayResults(){
+    $('.final').show();
+    let array = "";
+    const pass = [
+        "You really know your booze",
+        "images/correctAnswer.jpg"
+    ];
+    const fail = [
+        "At least you kinda tried.",
+        "images/wrongAnswer.jpg",
+        "Take ${shots} for your sort effort"
+    ];
+    if (shots > 2){
+        array = pass;
+    }else{
+        array = fail;
+    }
+    return $('.final').html(`
+        <h3>${array[0]}</h3>
+            <img src= "${array[1]}" alt= "${array[2]}" class= "images">
+            <h3>You got ${shots} / 5</h3>
+            <button type= "submit" class= "restartButton button>Restart</button>
+    `)
+}
+
+// restarts the quiz
+function restartQuiz(){
+    $('.theBar').on('click', 'restartButton', function(event){
+        event.preventDefault();
+        resetQuiz();
+        $('.altBox').hide();
+        $('.beginQuiz').show();
+    });
+}
+
+function handleQuizApp() {
+    startQuiz();
+    renderQuestions();
+    displayResults();
+    restartQuiz();
+}
+
+$(handleQuizApp);
---
+++ store.js
@@ -0,0 +1,55 @@
+const STORE = [//1
+    {
+        question: "What is this glass called? <img src='images/glassware.jpg' alt= 'glassware' width='150px'>",
+        answer: [
+            "Margarita glass",
+            "Snifter Glass",
+            "Nick and Nora Glass",
+            "Coupe Glass"],
+        correctAnswer: "Snift Glass"
+    },
+    //2
+    {
+        question: "Where did 'Tiki' originate?",
+        answer:[
+            "The US",
+            "England",
+            "Polynesia",
+            "Canada"
+        ],
+        correctAnswer: "The US"
+    },
+    //3
+    {
+        question: "What does the term 'up' mean?",
+        answer:[
+            "Shake the cocktail then serve without ice",
+            "Serve the cocktail in a collins glass without ice",
+            "Stir the cocktail then serve with ice",
+            "Serve the cocktail in a martini glass without ice",
+        ],
+        CorrectAnswer:"Serve the cocktail in a martini glass without ice"
+    },
+    //4
+    {
+        question: "Are all distilled spirit gluten-free?",
+        answer:[
+            "No, not if distilled from grain",
+            "Yes, as long as there are not additives after distillation",
+            "No, the gluten molecule survives distillation",
+            "Yes, distilled spirits are always gluten-free"
+        ],
+        correctAnswer:"Yes, as long as there are not additives after distillation"
+    },
+    //5
+    {
+        question: "What is the spirit in a Caipirinha?",
+        answer:[
+            "Vodka",
+            "Genever",
+            "Cachaca",
+            "Chareau"
+        ],
+        correctAnswer: "Cachaca"
+    }
+];
