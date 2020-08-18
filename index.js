b626c14a2ab1e6636da58f992c83f7b9c06c108f
Bridgette-theWebMaster <69090403+Bridgette-theWebMaster@users.noreply.github.com>
2020-08-18 16:21:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,155 +1,159 @@
 'use strict';
-// defining a few variable used throughout
+//definin a few variables
 let shots = 5;
 let questionNum = 0;
-let currentQuestion = 0;
 
-// when the user clicks the begin button
+// when the user clicks the begin button start the quiz
 function startQuiz() {
-    $('.altBox').hide();
-    $('.beginQuiz').on('click', '.startButton', function (event){
-        $('.startButton').hide();
-        $('.questionNum').text(1);
-        $('.questionBox').show();
-        $('.questionBox').prepend(renderQuestions());
-    });
-}
-// display question
-function renderQuestions(){
-    if(questionNum < STORE.length){
-        return questionForm(questionNum);
-    }else{
-        $('.questionBox').hide();
-        endQuiz();
-        $('.questionNum').text(5);
-    }
-}
-
-// display question number and shots to take
-function updateQuestion(){
-    questionNum++;
-    $('.questionNum').text(questionNum + 1);
-}
-function updateShots(){    
-    shots--;
-    $('.shots').text(shots);
-}
-
-function resetQuiz(){
-    shots = 5;
-    questionNum = 0;
-    $('.shots').text(5);
-    $('.questionNum').text(0);
-}
-
-function submitAnswer(){
-    $(".theBar").on('submit', function(event){
-        event.preventDefault();
-        $('.altBox').hide();
-        $('.response').show();
-        let selected = $('input:checked');
-        let answer = selected.val();
-        let correct = STORE[questionNum].correctAnswer;
-        if (answer === correct){
-            correctAnswer();
-        }else{
-            wrongAnswer();
-        }
-    });
-}
-
-// html question form
-function questionForm(questionIndex){
-    let formMaker = $(`<form>
-    <fieldset>
-        <legend class= "questionText">${STORE[questionIndex].question}
-        </legend>
-    </fieldset>
-</form>`)
-    let answerSelector = $(formMaker).find('fieldset');
-    STORE[questionIndex].answer.forEach(function (answerValue, answerIndex){
-        $(`<label class="bookMark" for=${answerIndex}">
-        <input class= "radio" type= "radio" id= "${answerIndex}" value= "${answerValue}" name= "answer" required>
-        <span>${answerValue}</span>
-        </label>`).appendTo(answerSelector);
-    });
-    $(`<button type= "submit" class= "submitButton button">Submit</button>`).appendTo(answerSelector);
-    return formMaker;
-}
-// correct and incorrect reponse
-function correctAnswer(){
-    $('.response').html(`
-        <h3>You're right!</h3>
-        <button type= "button" class= "nextButton button">Next</button>
-    `);
-}
-function wrongAnswer(){
-    $('.reponse').html(`
-        <h3>Your answer is not correct.</h3>
-        <p>The correct answer is:</p>
-        <p>${STORE[questionNum].correctAnswer}</p>
-        <button type= "button" class= "nextButton button">Next</button>
-    `);    
-    updateShots();
-}
-
-// move on to the next question
-function nextQuestion(){
-    $('.theBar').on('click', 'nextButton', function(event){
-        $('.altBox').hide();
-        $('.questionBox').show();
-        updateQuestionNum();
-        $('.questionBox form').replaceWith(renderQuestions());
-    });
-}
-
-// check progression throughout the quiz
-function quizProgession(){
-  $('body').on('click','.nextButton', (event) => {
-    STORE.currentQuestion === STORE.questions.length?displayResults() : renderQuestions();
+  $('.altBox').hide();
+  $('.beginQuiz').on('click', '.startButton', function (event) {
+    $('.beginQuiz').hide();
+    $('.questionNum').text(1);
+    $('.questionBox').show();
+    $('.questionBox').prepend(renderQuestion());
   });
 }
 
-// display results
-function displayResults(){
-    $('.final').show();
-    let array = "";
+// display current question
+function renderQuestion() {
+  if (questionNum < STORE.length) {
+    return createQuestion(questionNum);
+  } else {
+    $('.questionBox').hide();
+    finalScore();
+    $('.questionNum').text(5);
+  }
+}
+
+// update shots to be taken with each right or wrong answer
+//update question
+function updateScore() {
+  shots--;
+  $('.shots').text(shots);
+}
+function updateQuestionNumber() {
+  questionNum++;
+  $('.questionNum').text(questionNum + 1);
+}
+
+// reset question number and shots to be taken
+function resetQuiz() {
+  shots = 5;
+  questionNumber = 0;
+  $('.shots').text(0);
+  $('.questionNum').text(0);
+}
+
+
+
+//submits a selected answer and return appropriate response
+function submitAnswer() {
+  $('.theBar').on('submit', function (event) {
+    event.preventDefault();
+    $('.altBox').hide();
+    $('.response').show();
+    let selected = $('input:checked');
+    let answer = selected.val();
+    let correct = STORE[questionNum].correctAnswer;
+    if (answer === correct) {
+      correctAnswer();
+    } else {
+      wrongAnswer();
+    }
+  });
+}
+
+// html question form
+function createQuestion(questionIndex) {
+  let formMaker = $(`<form>
+    <fieldset>
+      <legend class="questionText">${STORE[questionIndex].question}</legend>
+    </fieldset>
+  </form>`)
+
+  let fieldSelector = $(formMaker).find('fieldset');
+
+  STORE[questionIndex].answer.forEach(function (answerValue, answerIndex) {
+    $(`<label class="bookMark" for="${answerIndex}">
+        <input class="radio" type="radio" id="${answerIndex}" value="${answerValue}" name="answer" required>
+        <span>${answerValue}</span>
+      </label>
+      `).appendTo(fieldSelector);
+  });
+  $(`<button type="submit" class="submitButton button"> Submit</button > `).appendTo(fieldSelector);
+  return formMaker;
+}
+
+// correct and incorrect response
+function correctAnswer() {
+  $('.response').html(
+    `<h3>Your answer is correct!</h3>
+    <button type="button" class="nextButton 
+    button">Next</button>`
+  );
+  updateScore();
+}
+function wrongAnswer() {
+  $('.response').html(
+    `<h3>That's the wrong answer...</h3>
+    <p class="bookMark">It's actually:</p>
+    <p class="bookMark">${STORE[questionNum].correctAnswer}</p>
+    <button type="button" class="nextButton button">Next</button>`
+  );
+}
+
+// move on to the next question
+function nextQuestion() {
+  $('.theBar').on('click', '.nextButton', function (event) { 
+    $('.altBox').hide();
+    $('.questionBox').show();
+    updateQuestionNumber();
+    $('.questionBox form').replaceWith(renderQuestion());
+  });
+}
+
+//display results
+function finalScore() {
+  $('.final').show();
+    let array = [];
     const pass = [
         "You really know your booze",
-        "correctAnswer.jpg"
+        "images/correctAnswer.jpg"
     ];
     const fail = [
         "At least you kinda tried.",
-        "wrongAnswer.jpg",
-        "Take ${shots} for your sort effort"
+        "images/wrongAnswer.jpg"
     ];
-    if (shots > 2){
-        array = pass;
-    }else{
+    if (shots > 3){
         array = fail;
+    }else{
+        array = pass;
     }
     return $('.final').html(`
         <h3>${array[0]}</h3>
-            <img src= "${array[1]}" alt= "${array[2]}" class= "images">
-            <h3>You got ${shots} / 5</h3>
+            <img src= "${array[1]}"class= "images">
+            <h3>Take ${shots} shots for your effort.</h3>
             <button type= "submit" class= "restartButton button>Restart</button>
     `)
 }
 
 // restarts the quiz
-function restartQuiz(){
-    $('.theBar').on('click', 'restartButton', function(event){
-        event.preventDefault();
-        resetQuiz();
-        $('.altBox').hide();
-        $('.beginQuiz').show();
-    });
+function restartQuiz() {
+  $('.theBar').on('click', '.restartButton', function (event) {
+    event.preventDefault();
+    resetQuiz();
+    $('.altBox').hide();
+    $('.startQuiz').show();
+  });
 }
 
-function handleQuizApp() {
-    startQuiz();
-    renderQuestions();
-    restartQuiz();
+//runs the functions
+function makeQuizApp() {
+  startQuiz();
+  renderQuestion();
+  submitAnswer();
+  nextQuestion();
+  restartQuiz();
 }
 
-$(handleQuizApp);
+$(makeQuizApp);
